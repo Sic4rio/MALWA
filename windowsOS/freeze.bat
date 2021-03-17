8161f27847bce065312055c052a1ef351eda06f4
MUKESHKUMAR2001 <mukeshkumarckarak@gmail.com>
2021-03-17 16:48:54 +0530

    files updated

---
+++ windowsOS/freeze.bat
@@ -0,0 +1,10 @@
+@echo off
+:a
+start www.youtube.com
+start www.facebook.com
+start www.amazon.com
+start www.earth.google.com
+start www.solarsystem.appzend.net
+start www.cybermap.kaspersky.com
+start www.threatmap.checkpoint.com
+goto :a
